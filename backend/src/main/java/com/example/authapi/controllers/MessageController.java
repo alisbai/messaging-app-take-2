3ecb434f3dcdea6524baed903dcd223c71c896e2
alisbai <ali.sbai.96@gmail.com>
2024-01-10 21:59:28 +0100

    .

--- backend/src/main/java/com/example/authapi/controllers/MessageController.java
+++ backend/src/main/java/com/example/authapi/controllers/MessageController.java
@@ -1,2 +1,45 @@
-package com.example.authapi.controllers;public class MessageController {
+package com.example.authapi.controllers;
+
+import com.example.authapi.entities.Message;
+import com.example.authapi.responses.UserResponse;
+import com.example.authapi.services.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController("api/v1/messages")
+
+public class MessageController {
+    private MessageService messageService;
+
+    public MessageController(){}
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+    @GetMapping("/index")
+    public ResponseEntity<List<Map>> geMessages() {
+        List<Message> messages = messageService.findAllMessages();
+        List<Map> response = new ArrayList<>();
+        for(Message message: messages) {
+            Map<String, Object> payload = new HashMap<>();
+            payload.put("message", message);
+            UserResponse userResponse = UserResponse.builder()
+                    .id(message.getUser().getId())
+                    .fullName(message.getUser().getFullName())
+                    .email(message.getUser().getEmail())
+                    .isOnline(message.getUser().getIsOnline())
+                    .build();
+            payload.put("user", userResponse);
+            response.add(payload);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
--- backend/src/main/java/com/example/authapi/entities/Message.java
+++ backend/src/main/java/com/example/authapi/entities/Message.java
@@ -35,4 +35,32 @@ public class Message {
     public Integer getId() {
         return id;
     }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
